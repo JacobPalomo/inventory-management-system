cb3f01fe874e3e2205877f3e23e80a40d4827a6f
Jacob Palomo <jacob.palomo248@gmail.com>
2026-03-18 23:40:53 -0600

    fix: auth security to register new users

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -8,36 +8,9 @@
 import { Router } from 'express'
 import { register, login } from './auth.controller'
 import { authorizeRoles } from '../../middlewares/role.middleware'
+import { authMiddleware } from '../../middlewares/auth.middleware'
 
 const router = Router()
-
-/**
- * @swagger
- * /api/auth/register:
- *   post:
- *     summary: Registrar nuevo usuario
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           example:
- *             name: Jacob
- *             email: jacob@test.com
- *             password: 123456
- *     responses:
- *       201:
- *         description: Usuario creado
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       400:
- *         description: Usuario ya existe o datos inválidos
- *       403:
- *         $ref: '#/components/responses/ForbiddenError'
- */
-router.post('/register', authorizeRoles('ADMIN', 'EDITOR'), register)
 
 /**
  * @swagger
@@ -51,7 +24,7 @@ router.post('/register', authorizeRoles('ADMIN', 'EDITOR'), register)
  *         application/json:
  *           example:
  *             email: admin@test.com
- *             password: 123456
+ *             password: "123456"
  *     responses:
  *       200:
  *         description: Login exitoso
@@ -61,9 +34,41 @@ router.post('/register', authorizeRoles('ADMIN', 'EDITOR'), register)
  *               $ref: '#/components/schemas/AuthResponse'
  *       400:
  *         $ref: '#/components/responses/InvalidCredentialsError'
+ */
+router.post('/login', login)
+
+router.use(authMiddleware)
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Registrar nuevo usuario
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             name: Jacob
+ *             email: jacob@test.com
+ *             password: "123456"
+ *     responses:
+ *       201:
+ *         description: Usuario creado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         $ref: '#/components/responses/InvalidBodyError'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
  *       403:
  *         $ref: '#/components/responses/ForbiddenError'
  */
-router.post('/login', login)
+router.post('/register', authorizeRoles('ADMIN'), register)
 
 export default router
