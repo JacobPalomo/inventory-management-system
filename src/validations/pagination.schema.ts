adcb18882180cd731ed0f55869822deb5354758e
Jacob Palomo <jacob.palomo248@gmail.com>
2026-03-26 14:26:05 -0600

    refactor(pagination): introduce base pagination validation schema

--- src/validations/pagination.schema.ts
+++ src/validations/pagination.schema.ts
@@ -1,11 +1,22 @@
 import z from 'zod'
-import { TZodPaginationQuery } from '../types/pagination'
 
-export const validatePaginateQuerySchema = z.object<TZodPaginationQuery>({
-	page: z
-		.int('Debe ser un número entero')
-		.nonnegative('No puede ser menor que 0'),
-	limit: z
-		.int('Debe ser un número entero')
-		.nonnegative('No puede ser menor que 0'),
+export const paginationSchema = z.object({
+	page: z.coerce
+		.number('No es número válido')
+		.int('No es un número entero')
+		.min(1, 'El valor mínimo permitido es 1')
+		.default(1),
+
+	limit: z.coerce
+		.number('No es número válido')
+		.int('No es un número entero')
+		.min(1, 'El valor mínimo permitido es 1')
+		.max(100, 'El valor máximo permitido es 100')
+		.default(10),
 })
+
+export type TPaginationQuery = z.infer<typeof paginationSchema>
+
+export const withPagination = <T extends z.ZodRawShape>(
+	schema: z.ZodObject<T>,
+) => schema.extend(paginationSchema.shape)
