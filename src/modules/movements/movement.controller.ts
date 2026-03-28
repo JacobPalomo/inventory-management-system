2f14327c20ea2689739d6d5cb230aedc8edfc876
Jacob Palomo <jacob.palomo248@gmail.com>
2026-03-27 18:31:24 -0600

    refactor(movements): simplify movement controllers and routes

--- src/modules/movements/movement.controller.ts
+++ src/modules/movements/movement.controller.ts
@@ -2,48 +2,35 @@ import { NextFunction, Response } from 'express'
 import { AuthRequest } from '../../middlewares/auth.middleware'
 import { createMovementService, getMovementsService } from './movement.service'
 import { movementQuerySchema, movementSchema } from './movement.schema'
+import { MovementType } from '@prisma/client'
+
+const createMovement = async (
+	req: AuthRequest,
+	res: Response,
+	next: NextFunction,
+	type: MovementType,
+) => {
+	try {
+		const validated = movementSchema.parse(req.body)
+		const result = await createMovementService(req.user.id, type, validated)
+
+		res.status(201).json(result)
+	} catch (error) {
+		next(error)
+	}
+}
 
 export const createEntry = async (
 	req: AuthRequest,
 	res: Response,
 	next: NextFunction,
-) => {
-	try {
-		const { productId, quantity } = movementSchema.parse(req.body)
-
-		const result = await createMovementService(
-			req.user.id,
-			'IN',
-			productId,
-			quantity,
-		)
-
-		res.status(201).json(result)
-	} catch (error) {
-		next(error)
-	}
-}
+) => createMovement(req, res, next, MovementType.IN)
 
 export const createExit = async (
 	req: AuthRequest,
 	res: Response,
 	next: NextFunction,
-) => {
-	try {
-		const { productId, quantity } = movementSchema.parse(req.body)
-
-		const result = await createMovementService(
-			req.user.id,
-			'OUT',
-			productId,
-			quantity,
-		)
-
-		res.status(201).json(result)
-	} catch (error) {
-		next(error)
-	}
-}
+) => createMovement(req, res, next, MovementType.OUT)
 
 export const getMovements = async (
 	req: AuthRequest,
